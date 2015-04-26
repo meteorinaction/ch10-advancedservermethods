e495a36db3d3d57eaa7f7ceb15f8f20027bfaf29
Stephan Hochhaus <stephan@yauh.de>
2015-04-26 18:41:27 +0200

    limit uploads to images only

---
+++ app/client/upload.js
@@ -0,0 +1,20 @@
+Template.upload.events({
+  'change #file-upload': function (event, template) {
+    console.log('base64');
+    var file = event.target.files[0];
+    var name = event.target.files[0].name;
+    if (!file) return;
+
+    // Only process image files.
+    if (!file.type.match('image.*')) {
+      alert('Only image files are allowed');
+      return;
+    }
+    var reader = new FileReader(); //create a reader according to HTML5 File API
+    reader.onload = function (file) {
+      var result = reader.result;
+      Meteor.call('saveFile', name, result);
+    }
+    reader.readAsDataURL(file); //read the file as base64
+  }
+});
